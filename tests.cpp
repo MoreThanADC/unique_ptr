e91384e2e74e398e06cc5387307f3b7c8b4182aa
Artur Michałowski <artur.michalowski93@gmail.com>
2021-10-10 17:05:23 +0200

    Add tests

--- tests.cpp
+++ tests.cpp
@@ -3,27 +3,55 @@
 #include <memory>
 
 
-TEST(UniquePointerTests, CreatePointerWithoutArgumentsAndExpectNullptrAfterCallGet) {
+TEST(UniquePointerTests, CreatePointerWithoutArgumentsAndExpectNullptr) {
     Unique<int> uniq;
     EXPECT_EQ(uniq.get(), nullptr);
 }
 
-TEST(UniquePointerTests, CreatePointerWithOneIntAsArgumentAndExpectAdressesAreTheSame) {
+TEST(UniquePointerTests, CreatePointerWithOneArgumentAndExpectAdressesAreTheSame) {
     int* number = new int(5);
     Unique<int> uniq{number};
     EXPECT_EQ(uniq.get(), number);
 }
 
-TEST(UniquePointerTests, CreatePointerWithOneArgumentUsingMoveSemanticAndExpectAdressesAreTheSame) {
+TEST(UniquePointerTests, CreatePonterWiithOneArgumentUsingMoveSemanticAndExpectAdressesAreTheSame) {
     int* number = new int(5);
     Unique<int> uniq{std::move(number)};
     EXPECT_EQ(uniq.get(), number);
 }
 
-// TEST(UniquePointerTests, UsingStarOperatorExpectAccessToTheElement) {
-//     int* number = new int(5);
-//     Unique<int> uniq{number};
-//     EXPECT_EQ(*uniq, 5);
-// }
+TEST(UniquePointerTests, UsingStarOperatorExpectAccessToTheElement) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    EXPECT_EQ(*uniq, *number);
+}
 
-// TEST(UniquePointerTests)
+TEST(UniquePointerTests, UsingRelaseExpectReturnedValueIsRawPointer) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    EXPECT_EQ(uniq.release(), number);
+    delete number;
+}
+
+TEST(UniquePointerTests, UsingRelaseExpectUniqeIsNullpointer) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    uniq.release();
+    EXPECT_EQ(uniq.get(), nullptr);
+    delete number;
+}
+
+TEST(UniquePointerTests, UsingResetWitoutArgumentsExpectNullptr) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    uniq.reset();
+    EXPECT_EQ(uniq.get(), nullptr);
+}
+
+TEST(UniquePointerTests, UsingResetWithArgumentExpectNewPointerInPlaceOldPointer) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    int* number2 = new int(10);
+    uniq.reset(number2);
+    EXPECT_EQ(uniq.get(), number2);
+}
