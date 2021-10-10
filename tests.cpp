f626d2ad07098c81bbaf6d6070dd718cb18888d1
Artur Michałowski <artur.michalowski93@gmail.com>
2021-10-10 15:40:18 +0200

    Add simple tests

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,29 @@
 #include "gtest/gtest.h"
 #include "Unique_ptr.cpp"
+#include <memory>
 
 
-TEST(PointerTests, GivenTwoPositiveNumbersAndRequirePositiveResult) {
-    
- 
+TEST(UniquePointerTests, CreatePointerWithoutArgumentsAndExpectNullptrAfterCallGet) {
+    Unique<int> uniq;
+    EXPECT_EQ(uniq.get(), nullptr);
 }
+
+TEST(UniquePointerTests, CreatePointerWithOneIntAsArgumentAndExpectAdressesAreTheSame) {
+    int* number = new int(5);
+    Unique<int> uniq{number};
+    EXPECT_EQ(uniq.get(), number);
+}
+
+TEST(UniquePointerTests, CreatePointerWithOneArgumentUsingMoveSemanticAndExpectAdressesAreTheSame) {
+    int* number = new int(5);
+    Unique<int> uniq{std::move(number)};
+    EXPECT_EQ(uniq.get(), number);
+}
+
+// TEST(UniquePointerTests, UsingStarOperatorExpectAccessToTheElement) {
+//     int* number = new int(5);
+//     Unique<int> uniq{number};
+//     EXPECT_EQ(*uniq, 5);
+// }
+
+// TEST(UniquePointerTests)
